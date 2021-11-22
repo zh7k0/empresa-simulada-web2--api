bfb64443768cdfb020a7a66b9283221db927e61f
zh7k0 <6224758+zh7k0@users.noreply.github.com>
2021-11-22 10:53:00 -0300

    Agregadas rutas para crear categorias

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoriaController;
 use App\Http\Controllers\EmpresaFeriaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -16,8 +17,8 @@ use App\Http\Controllers\EventoController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+
 });
 
 Route::get('/feria', [EventoController::class, 'obtenerFeria']);
@@ -29,3 +30,10 @@ Route::post('/feria/empresas', [EmpresaFeriaController::class, 'crear']);
 Route::get('/feria/empresas/{empresa}', [EmpresaFeriaController::class, 'mostrar']);
 Route::put('/feria/empresas/{empresa}', [EmpresaFeriaController::class, 'actualizar']);
 Route::delete('/feria/empresas/{empresa}', [EmpresaFeriaController::class, 'eliminar']);
+
+Route::get('/feria/visibilidad', [EventoController::class, 'esVisibleFeria']);
+
+Route::get('/categorias', [CategoriaController::class, 'obtenerCategorias']);
+Route::post('/categorias', [CategoriaController::class, 'crear']);
+Route::put('/categorias/{categoria}', [CategoriaController::class, 'actualizar']);
+Route::delete('/categorias/{categoria}', [CategoriaController::class, 'eliminar']);
