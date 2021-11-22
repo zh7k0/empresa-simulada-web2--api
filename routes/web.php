d2719e9c6d83e6224873ded77b72e35ab1e13407
zh7k0 <6224758+zh7k0@users.noreply.github.com>
2021-11-22 10:45:25 -0300

    Agregado sanctum y rutas

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
