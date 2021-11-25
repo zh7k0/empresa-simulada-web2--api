e0376b051679510e48ba570225f908dba0d5dced
zh7k0 <6224758+zh7k0@users.noreply.github.com>
2021-11-25 20:05:01 -0300

    Agregada rutas a sacntum

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\EmpresaController;
 use App\Http\Controllers\EmpresaFeriaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -18,22 +19,31 @@ use App\Http\Controllers\EventoController;
 */
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/feria', [EventoController::class, 'guardarFeria']);
+    Route::put('/feria/{feria}', [EventoController::class, 'actualizarFeria']);
 
+    Route::post('/feria/empresas', [EmpresaFeriaController::class, 'crear']);
+    Route::put('/feria/empresas/{empresa}', [EmpresaFeriaController::class, 'actualizar']);
+    Route::delete('/feria/empresas/{empresa}', [EmpresaFeriaController::class, 'eliminar']);
+
+    Route::get('/categorias', [CategoriaController::class, 'obtenerCategorias']);
+    Route::post('/categorias', [CategoriaController::class, 'crear']);
+    Route::put('/categorias/{categoria}', [CategoriaController::class, 'actualizar']);
+    Route::delete('/categorias/{categoria}', [CategoriaController::class, 'eliminar']);
+
+    Route::post('/empresas', [EmpresaController::class, 'crear']);
+    Route::get('/empresas/{empresa}', [EmpresaController::class, 'mostrar']);
+    Route::put('/empresas/{empresa}', [EmpresaController::class, 'actualizar']);
+    Route::delete('/empresas/{empresa}', [EmpresaController::class, 'eliminar']);
 });
 
 Route::get('/feria', [EventoController::class, 'obtenerFeria']);
-Route::post('/feria', [EventoController::class, 'guardarFeria']);
-Route::put('/feria/{feria}', [EventoController::class, 'actualizarFeria']);
 
 Route::get('/feria/empresas', [EmpresaFeriaController::class, 'listarEmpresas']);
-Route::post('/feria/empresas', [EmpresaFeriaController::class, 'crear']);
 Route::get('/feria/empresas/{empresa}', [EmpresaFeriaController::class, 'mostrar']);
-Route::put('/feria/empresas/{empresa}', [EmpresaFeriaController::class, 'actualizar']);
-Route::delete('/feria/empresas/{empresa}', [EmpresaFeriaController::class, 'eliminar']);
 
 Route::get('/feria/visibilidad', [EventoController::class, 'esVisibleFeria']);
 
-Route::get('/categorias', [CategoriaController::class, 'obtenerCategorias']);
-Route::post('/categorias', [CategoriaController::class, 'crear']);
-Route::put('/categorias/{categoria}', [CategoriaController::class, 'actualizar']);
-Route::delete('/categorias/{categoria}', [CategoriaController::class, 'eliminar']);
+
+
+Route::get('/empresas', [EmpresaController::class, 'obtenerEmpresas']);
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout']);
