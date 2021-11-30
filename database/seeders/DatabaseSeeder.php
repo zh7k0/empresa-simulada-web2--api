492ad484e6a8d0f98406fe794b66a98202be726e
zh7k0 <6224758+zh7k0@users.noreply.github.com>
2021-11-30 10:34:18 -0300

    Agregado admin de prueba

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $user = new User();
+        $user->username = 'pepelota';
+        $user->password = Hash::make('pepelota');
+        $user->save();
     }
 }
